b4a08dd7d9f0fb4060cce7954941bbc47fa3789f
Vinicius Guedes <viniciusguedes@ibm.com>
2023-07-23 17:14:17 -0300

    feat: adicao de openfeign

---
+++ src/main/java/com/example/demo/jsonplaceholder/JsonPlaceholderClient.java
@@ -0,0 +1,16 @@
+package com.example.demo.jsonplaceholder;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(value = "jsonplaceholder", url = "https://jsonplaceholder.typicode.com/")
+public interface JsonPlaceholderClient {
+    @GetMapping("posts")
+    List<Post> getPosts();
+
+    @GetMapping("post/{postId}")
+    Post getPost(@PathVariable("postId") Integer id);
+}
