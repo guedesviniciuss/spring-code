af5919fb8c24fb935f757ddf4a87f5b70ecfb0d9
Vinicius Guedes <viniciusguedes@ibm.com>
2023-07-25 19:57:59 -0300

    feat: adicao de logger

--- src/main/java/com/example/demo/customer/CustomerService.java
+++ src/main/java/com/example/demo/customer/CustomerService.java
@@ -1,6 +1,8 @@
 package com.example.demo.customer;
 
 import com.example.demo.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,18 +13,24 @@ public class CustomerService {
 
     private final CustomerRepository customerRepository;
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
+
     @Autowired
     public CustomerService(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
 
     List<Customer> getCustomers() {
+        LOGGER.info("Find all users");
         List<Customer> customers = customerRepository.findAll();
         return customers;
     }
 
     Customer getCustomer(Long id) {
         return customerRepository.findById(id)
-                .orElseThrow(() -> new NotFoundException("customer with id " + id + " not available"));
+                .orElseThrow(() -> {
+                    LOGGER.error("Customer with id {} not found", id);
+                    return new NotFoundException("customer with id " + id + " not available");
+                });
     }
 }
