7a8cbbc00d687564931ec8728a5882058cd17696
Vinicius Guedes <viniciusguedes@ibm.com>
2023-07-28 17:14:03 -0300

    fix: maven wrapper

--- src/main/java/com/example/demo/customer/CustomerConfiguration.java
+++ src/main/java/com/example/demo/customer/CustomerConfiguration.java
@@ -1,9 +1,12 @@
 package com.example.demo.customer;
 
+import com.example.demo.infoapp.InfoApp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class CustomerConfiguration {
@@ -11,10 +14,18 @@ public class CustomerConfiguration {
     @Value("${app.useFakeRepository:false}")
     private Boolean useFakeRepository;
 
+    @Value("${info.company.name}")
+    private String companyName;
+
+    @Autowired
+    private Environment env;
+
     @Bean
-    CommandLineRunner commandLineRunner() {
+    CommandLineRunner commandLineRunner(InfoApp infoApp) {
         return args -> {
-            System.out.println("ENTROUUUUUUUU!!!!");
+            System.out.println("ENTROUUUUUUUU!!!!" + companyName);
+            System.out.println(env.getProperty("info.company.name"));
+            System.out.println(infoApp.toString());
         };
     }
 
