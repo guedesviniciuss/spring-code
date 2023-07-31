c6b1415186c22bcb716cb664aeacd5ad680132ee
Vinicius Guedes <viniciusguedes@ibm.com>
2023-07-30 23:14:39 -0300

    feat: conversao apiException para record

--- src/main/java/com/example/demo/exception/ApiException.java
+++ src/main/java/com/example/demo/exception/ApiException.java
@@ -1,16 +1,14 @@
 package com.example.demo.exception;
 
-import lombok.Data;
-import lombok.ToString;
 import org.springframework.http.HttpStatus;
 
 import java.time.ZonedDateTime;
 
-@ToString
-@Data
-public class ApiException {
-    private final String message;
-    private final Throwable trouble;
-    private final HttpStatus httpStatus;
-    private final ZonedDateTime zonedDateTime;
+public record ApiException (
+        String message,
+        Throwable trouble,
+        HttpStatus httpStatus,
+        ZonedDateTime zonedDateTime) {
 }
+
+// Record: private final field, getter for each field, public constructor, equalToHashCode, toString
